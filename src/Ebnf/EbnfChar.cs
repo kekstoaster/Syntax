52e95e1f5ff8f6e313277ad74caa4bf4946d0f03
Stefan Apostel <stefan.apostel@gmx.de>
2016-10-19 23:15:35 +0200

    organized folder structure

---
+++ src/Ebnf/EbnfChar.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+namespace Kekstoaster.Syntax
+{
+	public class EbnfChar:Ebnf
+	{
+		private char _char;
+
+		public EbnfChar (char c, ScopeType scopetype = ScopeType.Default) : base (scopetype)
+		{
+			this._char = c;
+			this.Label = c.ToString ();
+		}
+
+		internal override object MatchElement (Stream s, EbnfCompiler compiler)
+		{
+			long startPos = s.Position;
+			object result = null;
+
+			char next = '\0';
+			if (compiler.Encoder == null) {
+				int n = s.ReadByte ();
+				if (n == -1) {
+					ThrowElementException ();
+				}
+				next = (char)n;
+			} else {
+				try {
+					next = compiler.Encoder.NextChar (s);
+				} catch (EofException) {
+					ThrowElementException ();
+				}
+			}
+			//Debug.Write ((char)next);
+			if (next == this._char) {
+				SyntaxElement c;
+				c = new SyntaxText (Ebnf.DefaultParseAction, compiler, this._char);
+				c.Label = this.Label;
+				result = ParseResult (c, compiler);
+			} else {
+				if (this._error == null) {
+					throw new EbnfElementException ();
+				} else {
+					throw new EbnfElementException (string.Format (this._error, (char)next));
+				}
+			}
+
+			return result;
+		}
+
+		public override bool CanBeEmpty {
+			get {
+				return false;
+			}
+		}
+
+		public override bool IsGeneric {
+			get {
+				if (this.CompileAction == null) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		public override Ebnf Clone ()
+		{
+			EbnfChar n = new EbnfChar (this._char, this._scopeType);
+			n.CompileAction = this.CompileAction;
+			n.Label = this.Label;
+			n.ParseAction = this.ParseAction;
+			n._error = this._error;
+			return n;
+		}
+
+		internal override string ToString (int depth)
+		{
+			return this._char.ToString ();
+		}
+	}
+}
+
---
+++ src/Ebnf/EbnfExclusion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace Kekstoaster.Syntax
+{
+	public class EbnfExclusion:Ebnf
+	{
+		Ebnf _element;
+
+		public EbnfExclusion (Ebnf element, ScopeType scopetype = ScopeType.Default) : base (scopetype)
+		{
+			_element = element;
+		}
+
+		internal override object MatchElement (Stream s, EbnfCompiler compiler)
+		{
+			long startPos = s.Position;
+			object result = null;
+
+			object arg = null;
+
+			try {
+				arg = _element.MatchElement (s, compiler);
+			} catch (EbnfElementException) {
+				s.Position = startPos;					
+			}
+			if (arg != null) {
+				ThrowElementException ();
+			} else {					
+				result = IgnoredElement.Instance;
+			}
+
+			return result;
+		}
+
+		public override bool IsGeneric {
+			get {
+				if (this.CompileAction == null) {
+					return _element.IsGeneric;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		public override bool CanBeEmpty {
+			get {
+				return true;
+			}
+		}
+
+		public override Ebnf Clone ()
+		{
+			EbnfExclusion n = new EbnfExclusion (_element, this._scopeType);
+			n.CompileAction = this.CompileAction;
+			n.Label = this.Label;
+			n.ParseAction = this.ParseAction;
+			n._error = this._error;
+			return n;
+		}
+
+		internal override string ToString (int depth)
+		{
+			return "-" + _element.ToString (depth - 1);
+		}
+	}
+}
+
